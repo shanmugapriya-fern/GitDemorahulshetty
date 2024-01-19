a9d362742df996c95ad87e296183610d89b42119
Shanmugapriya Kandasamy <mohanpriya99421@gmail.com>
2024-01-19 14:12:56 +0530

    First commit

---
+++ src/armstrong.java
@@ -0,0 +1,29 @@
+
+public class armstrong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number  =152;
+		int orginal=number;
+		int rem;
+		int arms=0;
+		while(number>0)
+		{
+			rem=number%10;
+			arms=arms+(rem*rem*rem);
+			number=number/10;
+		}
+		if(arms==orginal)
+		{
+		System.out.println(" arms");
+	
+		}
+		else {
+			System.out.println("not arms");
+		}
+			
+		
+
+	}
+
+}
